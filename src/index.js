213988b0be7875fd91e0eee0b6322ea7da873d9e
Grant <3958482+Grantlyk@users.noreply.github.com>
2019-06-22 21:31:56 +0100

    Initial setup (#1)
    initial setup

---
+++ src/index.js
@@ -0,0 +1,32 @@
+export function errorHandler(err, req, res, next) {
+  if (res.headersSent) {
+    return next(err);
+  }
+
+  res.status(err.status || 500).json({
+    message: err.message || 'An error has occured',
+    code: err.status || 500,
+  });
+}
+
+export function enableCORS(req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header(
+    'Access-Control-Allow-Headers',
+    'Origin, X-Requested-With, X-XSRF-TOKEN, Content-Type, Accept, X-Access-Token',
+  );
+  res.header('Access-Control-Allow-Methods', 'OPTIONS, POST, PUT, DELETE');
+  next();
+}
+
+export function notFound(req, res) {
+  res.status(404).json({
+    message: 'Endpoint not Found',
+    code: 404,
+  });
+}
+
+export function supportCrossOrigin(req, res, next) {
+  res.status(200);
+  enableCORS(req, res, next);
+}
